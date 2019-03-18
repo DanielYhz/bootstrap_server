9ebb72c103387024558c21f4564ae9b3fd934147
Daniel Yang <achillesyang@hotmail.com>
2019-03-18 11:02:47 -0700

    init
    init

---
+++ src/main/java/cmpe273/group6/bootstrap_server/BootstrapServerApplication.java
@@ -0,0 +1,13 @@
+package cmpe273.group6.bootstrap_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmpe273/group6/bootstrap_server/BootstrapServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cmpe273.group6.bootstrap_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootstrapServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
